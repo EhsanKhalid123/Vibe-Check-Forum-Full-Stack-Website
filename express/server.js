2f8b040559aad3c8d572a27359307a2f3c6ae97e
--replace-all <ehsan.khalid123@yahoo.com>
2021-10-02 18:48:43 +1000

    Inital Setup

---
+++ express/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./src/database");
+
+// Database will be sync'ed in the background.
+db.sync();
+
+const app = express();
+
+// Parse requests of content-type - application/json.
+app.use(express.json());
+
+// Add CORS suport.
+app.use(cors());
+
+// Set port, listen for requests.
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
